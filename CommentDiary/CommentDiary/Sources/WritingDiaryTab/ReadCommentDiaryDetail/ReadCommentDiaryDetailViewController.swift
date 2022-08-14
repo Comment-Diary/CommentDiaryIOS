5183179b6482e96a03016248a33930588c7bfd66
ryuchanghwi <ckdgnl10@naver.com>
2022-08-14 17:19:34 +0900

    update

---
+++ CommentDiary/CommentDiary/Sources/WritingDiaryTab/ReadCommentDiaryDetail/ReadCommentDiaryDetailViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ReadCommentDiaryDetailViewController.swift
+//  CommentDiary
+//
+//  Created by 류창휘 on 2022/08/12.
+//
+
+import Foundation
+import UIKit
+
+class ReadCommentDiaryDetailViewController : UIViewController {
+    @IBOutlet weak var diaryTableView: UITableView!
+    @IBOutlet weak var navigationBarVIew: UIView!
+    @IBOutlet weak var navigationBarTItleLabel: UILabel!
+    @IBOutlet weak var navigationBarSeparateView: UIView!
+    
+//    enum hexColor {
+//        case backgroundColor
+//        case navigationTitleLabelColor
+//        case naviagationSeparateViewColor
+//        case dateLabelColor
+//        case titleLabelColor
+//        case contentLabelColor
+//
+//        func getHexColor() -> UIColor {
+//            switch self {
+//            case .backgroundColor:
+//                return UIColor(hex: 0xFDFCF9)
+//            case .navigationTitleLabelColor:
+//                return UIColor(hex: 0x4E4C49)
+//            case .naviagationSeparateViewColor:
+//                return
+//            case .dateLabelColor:
+//                return
+//            case .titleLabelColor:
+//                return
+//            case .contentLabelColor:
+//                return
+//            }
+//        }
+//    }
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    // MARK: - Actions
+}
